e51ac0bc1b4e51996836524e5bd19d7a4d54efb6
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2022-11-29 13:30:13 +0100

    refactor - inscription/connexion

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
 const charactersRouter = require('./routes/characters');
+const authsRouter = require('./routes/auths');
 
 const app = express();
 
@@ -14,6 +15,6 @@ app.use(cookieParser());
 
 app.use('/users', usersRouter);
 app.use('/characters', charactersRouter);
-
+app.use('/auths', authsRouter);
 
 module.exports = app;
