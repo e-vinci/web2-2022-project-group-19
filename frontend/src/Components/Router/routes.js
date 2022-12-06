be4725a3a826a1974d272b79b3532cb06ec93b5a
Clément Dededessuslesmoustier <90784495+ClementDedessus@users.noreply.github.com>
2022-12-06 12:48:51 +0100

    Update routes.js

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,13 +1,15 @@
-import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
+import UserPage from '../Pages/UserPage';
+import HomePage from '../Pages/HomePage';
 
 const routes = {
   '/': HomePage,
   '/login': LoginPage,
   '/register': RegisterPage,
   '/logout': Logout,
+  '/user' : UserPage,
 };
 
 export default routes;
