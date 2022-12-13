2945d2d29fa07fb4ecaff112e67ee48e9f32b267
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2022-12-13 15:34:10 +0100

    add - start of readAllUsers in MemberPages

--- frontend/src/Components/Pages/MemberPage.js
+++ frontend/src/Components/Pages/MemberPage.js
@@ -1,7 +1,19 @@
+// const path = require('node:path');
+// const { parse } = require('../../../../api/utils/json.js');
+
 const MemberPage = () => {
     const main = document.querySelector('main');
     main.innerHTML = `<h3>Member administration zone</h3>`;
+    
+    const jsonDbPath = path.join(__dirname, '../../../../api/data/users.json');
 
+    function readAllUsers() {
+ 
+        const users = parse(jsonDbPath);
+      
+        return users;
+    };
+    console.log(readAllUsers);
   };
 
 
