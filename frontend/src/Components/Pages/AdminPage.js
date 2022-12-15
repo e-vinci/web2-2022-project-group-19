739373ab895f6cda21c1c7b830cb7c3da6b054a9
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2022-12-15 14:39:05 +0100

    Refactor - toujours des erreurs sur l'affchage des membres -> AdminPages

--- frontend/src/Components/Pages/AdminPage.js
+++ frontend/src/Components/Pages/AdminPage.js
@@ -1,4 +1,5 @@
 import { clearPage } from '../../utils/render';
+ // problème ici
 import { getAllUsers, deleteOneUser } from '../../models/users';
 import Navbar from '../Navbar/Navbar';
 
@@ -17,32 +18,24 @@ function showUsersTable(){
     const users = getAllUsers();
     for(const user of users){
         const row = document.createElement('tr');
-    
         // create a table cell for each user property
         for (const key in user) {
             const cell = document.createElement('td');
             cell.innerText = user[key];
             row.appendChild(cell);
         }
-
         // add the delete button to the row
         const deleteButton = document.createElement('button');
         deleteButton.innerText = 'Delete';
         deleteButton.addEventListener('click', () => deleteOneUser(user.username));
         row.appendChild(deleteButton);
-
         // add the row to the table
         table.appendChild(row);
     }
-
     // append the table to the page component
     page.appendChild(table);
-
     // append the page component to the document
     document.body.appendChild(page);
-
 }
-
-
 
 export default AdminPage;
