e40354eb64380305f186c0f68adc784275590c50
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2022-12-18 23:04:59 +0100

    Add - cors

--- api/app.js
+++ api/app.js
@@ -12,6 +12,12 @@ const openaiRouter = require('./routes/openai');
 
 const app = express();
 
+const cors = require('cors');
+
+const corsOptions = {
+    origin: ['http://localhost:8080/', 'https://e-vinci.github.io/'],
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,14 +26,16 @@ app.use(
     fileUpload()
 );
 
-app.use("/uploads", express.static(__dirname + '/uploads'));
+app.use("/uploads",cors(corsOptions), express.static(__dirname + '/uploads'));
 
-app.use('/users', usersRouter);
-app.use('/characters', charactersRouter);
-app.use('/auths', authsRouter);
-app.use('/votes', votesRouter);
-app.use('/comments', commentsRouter);
-app.use('/openai', openaiRouter);
+app.use('/users',cors(corsOptions), usersRouter);
+app.use('/characters',cors(corsOptions), charactersRouter);
+app.use('/auths',cors(corsOptions), authsRouter);
+app.use('/votes',cors(corsOptions), votesRouter);
+app.use('/comments',cors(corsOptions), commentsRouter);
+app.use('/openai',cors(corsOptions), openaiRouter);
+
+
 
 
 module.exports = app;
